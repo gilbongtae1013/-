496ce9b3f1d99b2ea98daa4f66cfa3832508a180
gilbongtae1013 <gilbongtae@gmail.com>
2026-08-31 18:59:28 +0900

    글 업로드 완성

--- MteWorld/src/pages/Upload.jsx
+++ MteWorld/src/pages/Upload.jsx
@@ -1,17 +1,37 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useOutletContext, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
 export default function Upload() {
 
+    const { userPost, setUserPost } = useOutletContext();
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    const navigate = useNavigate();
+
+    const Upload = () => {
+        if(title === "" || content === "") {
+            alert("제목 또는 본문을 입력해주세요.");
+            return;
+        }
+        setUserPost([{ title: title, content: content, popular: 0 }, ...userPost])
+
+        alert("업로드 성공");
+        navigate('/');
+    }
+
     return (
         <div id='Upload-container'>
             <div id='Upload-header'>
-                <button id='Upload-upload'>업로드</button>
+                <button id='Upload-upload' onClick={Upload}>업로드</button>
             </div>
 
-            <input id='Upload-title' placeholder='제목을 입력하세요'/>
+            <input id='Upload-title' placeholder='제목을 입력하세요'
+            onChange={(e) => {setTitle(e.target.value)}} value={title}/>
 
-            <textarea id='Upload-content' placeholder='본문 입력'/>
+            <textarea id='Upload-content' placeholder='본문 입력'
+            onChange={(e)=> {setContent(e.target.value)}} value={content}/>
         </div>
     )
 }
--- MteWorld/src/pages/community.jsx
+++ MteWorld/src/pages/community.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useOutletContext } from 'react-router-dom';
 
 export default function Community() {
 
